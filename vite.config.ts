49c09b66033c712983c8464d8375a50ce95c8d87
Jonathan Lascano <jonathanlascano2393@gmail.com>
2025-09-21 11:20:27 -0700

    Update name in configurations

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jaji/',
+  base: '/beverage-pricing-tools/',
   plugins: [react()],
 })
